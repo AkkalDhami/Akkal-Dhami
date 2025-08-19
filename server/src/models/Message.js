2f8ac772e5a4f75ba6cd79be734a2cf461c32dcb
AkkalDhami <dhamiakkal21@gmail.com>
2025-08-19 14:53:23 +0545

    add multer

---
+++ server/src/models/Message.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+    message: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+export default mongoose.model('Message', messageSchema);
