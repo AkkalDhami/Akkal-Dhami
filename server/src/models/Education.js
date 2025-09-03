998d019c7821a1b10920a623c125d6a88de8e347
AkkalDhami <dhamiakkal21@gmail.com>
2025-09-03 18:22:14 +0545

    Create Education.js

---
+++ server/src/models/Education.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const educationSchema = new mongoose.Schema(
+    {
+        institution: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        degree: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        startDate: {
+            type: Date,
+            required: true,
+            trim: true
+        },
+        endDate: {
+            type: Date,
+            required: true,
+            trim: true
+        },
+        description: {
+            type: String,
+            trim: true
+        }
+    },
+    { timestamps: true }
+);
+
+const Education = mongoose.models.Education || mongoose.model('Education', educationSchema);
+
+export default Education;
