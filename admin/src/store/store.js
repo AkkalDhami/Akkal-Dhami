107bfa3cf080081f93bb4d01c48c263136b78827
AkkalDhami <dhamiakkal21@gmail.com>
2025-10-04 12:30:45 +0545

    Update store.js

--- admin/src/store/store.js
+++ admin/src/store/store.js
@@ -1,45 +1,49 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit";
 
-import themeReducer from "../features/theme/themeSlice"
-import uiReducer from "../features/ui/uiSlice"
+import themeReducer from "../features/theme/themeSlice";
+import uiReducer from "../features/ui/uiSlice";
 
-import projectReducer from "../features/project/projectSlice"
-import skillReducer from "../features/skill/skillSlice"
+import projectReducer from "../features/project/projectSlice";
+import skillReducer from "../features/skill/skillSlice";
 
-import authReducer from "../features/auth/authSlice"
-import authApi from "../features/auth/authApi"
+import authReducer from "../features/auth/authSlice";
+import authApi from "../features/auth/authApi";
 
-import projectApi from "../features/project/projectApi"
-import messageApi from "../features/messages/messageApi"
+import projectApi from "../features/project/projectApi";
+import messageApi from "../features/messages/messageApi";
 
-import skillApi from "../features/skill/skillApi"
-import eduApi from "../features/education/eduApi"
-import experienceApi from "../features/experience/experienceApi"
+import skillApi from "../features/skill/skillApi";
+import eduApi from "../features/education/eduApi";
+import experienceApi from "../features/experience/experienceApi";
+import aboutApi from "../features/about/aboutApi";
 
 export const store = configureStore({
-    reducer: {
-        theme: themeReducer,
-        ui: uiReducer,
-        projects: projectReducer,
-        skills: skillReducer,
-        auth: authReducer,
-        [authApi.reducerPath]: authApi.reducer,
-        [projectApi.reducerPath]: projectApi.reducer,
-        [skillApi.reducerPath]: skillApi.reducer,
-        [eduApi.reducerPath]: eduApi.reducer,
-        [experienceApi.reducerPath]: experienceApi.reducer,
-        [messageApi.reducerPath]: messageApi.reducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck: false
+  reducer: {
+    theme: themeReducer,
+    ui: uiReducer,
+    projects: projectReducer,
+    skills: skillReducer,
+    auth: authReducer,
+    [authApi.reducerPath]: authApi.reducer,
+    [projectApi.reducerPath]: projectApi.reducer,
+    [skillApi.reducerPath]: skillApi.reducer,
+    [eduApi.reducerPath]: eduApi.reducer,
+    [experienceApi.reducerPath]: experienceApi.reducer,
+    [messageApi.reducerPath]: messageApi.reducer,
+    [aboutApi.reducerPath]: aboutApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
     }).concat(
-        authApi.middleware,
-        skillApi.middleware,
-        eduApi.middleware,
-        experienceApi.middleware,
-        projectApi.middleware,
-        messageApi.middleware
+      authApi.middleware,
+      skillApi.middleware,
+      eduApi.middleware,
+      experienceApi.middleware,
+      projectApi.middleware,
+      messageApi.middleware,
+      aboutApi.middleware
     ),
-})
+});
 
-export default store
+export default store;
