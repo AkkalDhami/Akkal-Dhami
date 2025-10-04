1f036c60ec1c000cf4d91ccdc201a130fe85476c
AkkalDhami <dhamiakkal21@gmail.com>
2025-10-04 12:39:58 +0545

    add port

--- client/src/features/aboutApi.js
+++ client/src/features/aboutApi.js
@@ -1,20 +1,18 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 const aboutApi = createApi({
-    reducerPath: 'AboutApi',
-    baseQuery: fetchBaseQuery({ baseUrl: `${BASE_URL}/api/about` }),
-    tagTypes: ['About'],
-    endpoints: (builder) => ({
-        getMyContacts: builder.query({
-            query: () => '/'
-        })
+  reducerPath: "AboutApi",
+  baseQuery: fetchBaseQuery({ baseUrl: `${BASE_URL}/api/about` }),
+  tagTypes: ["About"],
+  endpoints: (builder) => ({
+    getMyContacts: builder.query({
+      query: () => "/",
     }),
+  }),
 });
 
-export const {
-    useGetMyContactsQuery
-} = aboutApi;
+export const { useGetMyContactsQuery } = aboutApi;
 
 export default aboutApi;
--- client/src/features/contactApi.js
+++ client/src/features/contactApi.js
@@ -1,25 +1,23 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 const contactApi = createApi({
-    reducerPath: 'contactApi',
-    baseQuery: fetchBaseQuery({ baseUrl: `${BASE_URL}/api/message` }),
-    tagTypes: ['Contact'],
-    endpoints: (builder) => ({
-        sendMessage: builder.mutation({
-            query: (data) => ({
-                url: '/send',
-                method: 'POST',
-                body: data,
-            }),
-            invalidatesTags: [{ type: 'Contact', id: 'LIST' }],
-        }),
+  reducerPath: "contactApi",
+  baseQuery: fetchBaseQuery({ baseUrl: `${BASE_URL}/api/message` }),
+  tagTypes: ["Contact"],
+  endpoints: (builder) => ({
+    sendMessage: builder.mutation({
+      query: (data) => ({
+        url: "/send",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: [{ type: "Contact", id: "LIST" }],
     }),
+  }),
 });
 
-export const {
-    useSendMessageMutation,
-} = contactApi;
+export const { useSendMessageMutation } = contactApi;
 
 export default contactApi;
